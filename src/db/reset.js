55b322d019b375b0be697b4b11fb3af5d812b629
Rajesh Kumar <rajakumarlohano@gmail.com>
2026-05-28 23:34:14 +0800

    delete src

--- src/db/reset.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-
-const DATA_DIR = path.resolve(process.env.DATA_DIR || './data');
-const STORE_FILE = path.resolve(process.env.STORE_FILE || path.join(DATA_DIR, 'conversationos.store.json'));
-
-if (fs.existsSync(STORE_FILE)) fs.unlinkSync(STORE_FILE);
-console.log(`Reset store: ${STORE_FILE}`);
